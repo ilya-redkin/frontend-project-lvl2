b3a74021bd556f0b331553a2c2015c7724943b41
Ilya Redkin <ilya.redking@gmail.com>
2021-08-25 17:55:12 -0400

    json files file1 and file2 added

--- gendiff.js
+++ gendiff.js
@@ -4,5 +4,5 @@ program.version('0.0.1');
 program
     .option('-f, --format <type>', 'output format');
 program
-    .description('Compares two configuration files and show a difference.')
+    .description('Compares two configuration files and shows a difference.')
 program.parse(process.argv);
