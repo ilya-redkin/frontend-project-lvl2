7acde082bf0da783ca05ea0e57264ea3db76c1d4
Ilya Redkin <ilya.redking@gmail.com>
2021-09-01 22:43:31 -0400

    step 3 is done

--- gendiff.js
+++ gendiff.js
@@ -2,9 +2,11 @@
 
 
 const program = require('commander');
-const fs = require('fs');
-const _ = require('lodash');
-const path = require ('path');
+const compare = require('./index.js');
+
+// compare('./file1.json', './file2.json');
+
+
 program.version('0.0.1');
 program
     .option('-f, --format <type>', 'output format');
@@ -14,36 +16,7 @@ program
     .argument('<file1>', 'first file to compare')
     .argument('<file2>', 'second file to compare')
     .description('compares two configuration files and shows a difference')
-    .action((file1, file2) => {
-        const data1 = JSON.parse(fs.readFileSync(`${path.resolve(__dirname, file1)}`, 'utf8'));
-    const data2 = JSON.parse(fs.readFileSync(`${path.resolve(__dirname, file2)}`, 'utf8'));
-    
-    const listOfKeys = _.union(Object.keys(data1), Object.keys(data2)).sort();
-    let output = '';
-    
-    for (const key of listOfKeys) {
-    if (!data2.hasOwnProperty(key)) {
-        output = output + `
-        - ${key}: ${data1[key]}`
-    }
-    if (!data1.hasOwnProperty(key)) {
-        output = output + `
-        + ${key}: ${data2[key]}`
-    }
-    if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] === data2[key]) {
-        output = output + `
-          ${key}: ${data1[key]}`
-    }
-    if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] !== data2[key]) {
-        output = output + `
-        - ${key}: ${data1[key]}
-        + ${key}: ${data2[key]}`
-    }
-    }
-    output = `{${output}
-    }`;
-    console.log(output);
-    });
+    .action((file1, file2) => compare(file1, file2));
     
 program.parse(process.argv);
 
---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const _ = require('lodash');
+const path = require ('path');
+
+const compare = (file1, file2) => {
+
+    const data1 = JSON.parse(fs.readFileSync(`${path.resolve(__dirname, file1)}`, 'utf8'));
+    const data2 = JSON.parse(fs.readFileSync(`${path.resolve(__dirname, file2)}`, 'utf8'));
+    
+    const listOfKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+    let output = '';
+    
+    for (const key of listOfKeys) {
+    if (!data2.hasOwnProperty(key)) {
+        output = output + `
+        - ${key}: ${data1[key]}`
+    }
+    if (!data1.hasOwnProperty(key)) {
+        output = output + `
+        + ${key}: ${data2[key]}`
+    }
+    if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] === data2[key]) {
+        output = output + `
+          ${key}: ${data1[key]}`
+    }
+    if (data1.hasOwnProperty(key) && data2.hasOwnProperty(key) && data1[key] !== data2[key]) {
+        output = output + `
+        - ${key}: ${data1[key]}
+        + ${key}: ${data2[key]}`
+    }
+    }
+    output = `{${output}
+    }`;
+    console.log(output);
+    
+};
+
+module.exports = compare;
