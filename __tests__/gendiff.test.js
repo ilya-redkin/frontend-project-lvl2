1a0655be5161c2a71b59e5fcce144a378fbd1b5a
Ilya Redkin <ilya.redking@gmail.com>
2021-10-10 10:49:05 -0400

    fixtures added

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -45,3 +45,53 @@ test('compare', async () => {
           timeout: 50
 }`);
 });
+
+test('compare', async () => {
+  const file1 = `${__dirname}/../__fixtures__/testfile3.json`;
+  const file2 = `${__dirname}/../__fixtures__/testfile4.json`;
+  expect(compare(file1, file2)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+  
+});
