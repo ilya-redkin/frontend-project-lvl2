99a8e37c78eb34d779739ac7ef822442169b77cd
Ilya Redkin <ilya.redking@gmail.com>
2021-12-02 16:59:55 -0500

    troubleshooting 1

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,20 @@
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
 
-const formatters = {
-  plain: makePlain,
-  stylish: makeStylish,
-  json: JSON.stringify,
-};
+// const formatters = {
+//   plain: makePlain,
+//   stylish: makeStylish,
+//   json: JSON.stringify,
+// };
 
-const render = (ast, format) => formatters[format](ast);
+const render = (ast, format) => {
+  if (format === 'plain') {
+    return makePlain(ast);
+  }
+  if (format === 'stylish') {
+    return makeStylish(ast);
+  }
+  return JSON.stringify(ast);
+};
 
 export default render;
