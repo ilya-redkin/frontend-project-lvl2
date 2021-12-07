5d34505a98261f921fc5d45a40c75793dab99bb2
Ilya Redkin <ilya.redking@gmail.com>
2021-12-06 23:17:21 -0500

    refactoring-1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,65 +1,59 @@
 import _ from 'lodash';
 
-const makeStylish = (arr, replacer = '    ', spacesCount = 1) => {
-  if (!Array.isArray(arr)) { return 'The file is neither .json nor .yaml'; }
-  const iter = (data, depth) => {
-    const currentIndent = replacer.toString().repeat(depth * spacesCount);
-    const bracketIndent = replacer.toString().repeat((depth - 1) * spacesCount);
+const replacer = '    ';
+const replacerCount = 1;
 
-    const stringify = (value) => {
-      const iter2 = (currentValue, stringifyDepth) => {
-        const stringifyCurrentIndent = replacer
-          .repeat((stringifyDepth + 1) * spacesCount);
-        const stringifyBracketIndent = replacer.toString().repeat(stringifyDepth * spacesCount);
-        if (currentValue === null) {
-          return null;
-        }
-        if (!_.isObject(currentValue)) {
-          return currentValue.toString();
-        }
-        const lines = Object
-          .entries(currentValue)
-          .map(([key, val]) => `${stringifyCurrentIndent}${key}: ${iter2(val, stringifyDepth + 1)}`);
-        return [
-          '{',
-          ...lines,
-          `${stringifyBracketIndent}}`,
-        ].join('\n');
-      };
-      return iter2(value, depth);
-    };
-    const lines = data
-      .map((currentValue) => {
-        if (currentValue.type === 'removed') {
-          return `${currentIndent.slice(0, -2)}- ${currentValue.key}: ${stringify(currentValue.value)}`;
-        }
-
-        if (currentValue.type === 'added') {
-          return `${currentIndent.slice(0, -2)}+ ${currentValue.key}: ${stringify(currentValue.value)}`;
-        }
-
-        if (currentValue.type === 'unchanged') {
-          return `${currentIndent.slice(0, -2)}  ${currentValue.key}: ${stringify(currentValue.value)}`;
-        }
-
-        if (currentValue.type === 'changed') {
-          return `${currentIndent.slice(0, -2)}- ${currentValue.key}: ${stringify(currentValue.oldValue)}\n${currentIndent.slice(0, -2)}+ ${currentValue.key}: ${stringify(currentValue.newValue)}`;
-        }
-
-        if (currentValue.type === 'changedInside') {
-          return `${currentIndent.slice(0, -2)}  ${currentValue.key}: ${iter(currentValue.children, depth + 1)}`;
-        }
-        return 0;
-      });
-
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(arr, 1);
+const handleCurrentValue = (currentValue, depth) => {
+  const currentIndent = replacer.repeat((depth + 1) * replacerCount);
+  const bracketIndent = replacer.toString().repeat(depth * replacerCount);
+  if (currentValue === null) {
+    return null;
+  }
+  if (!_.isObject(currentValue)) {
+    return currentValue.toString();
+  }
+  const lines = Object
+    .entries(currentValue)
+    .map(([key, val]) => `${currentIndent}${key}: ${handleCurrentValue(val, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
 };
+
+const iterateThroughValues = (data, depth) => {
+  const currentIndent = replacer.toString().repeat(depth * replacerCount);
+  const bracketIndent = replacer.toString().repeat((depth - 1) * replacerCount);
+
+  const lines = data
+    .map((currentValue) => {
+      if (currentValue.type === 'removed') {
+        return `${currentIndent.slice(0, -2)}- ${currentValue.key}: ${handleCurrentValue(currentValue.value, depth)}`;
+      }
+
+      if (currentValue.type === 'added') {
+        return `${currentIndent.slice(0, -2)}+ ${currentValue.key}: ${handleCurrentValue(currentValue.value, depth)}`;
+      }
+
+      if (currentValue.type === 'unchanged') {
+        return `${currentIndent.slice(0, -2)}  ${currentValue.key}: ${handleCurrentValue(currentValue.value, depth)}`;
+      }
+
+      if (currentValue.type === 'changed') {
+        return `${currentIndent.slice(0, -2)}- ${currentValue.key}: ${handleCurrentValue(currentValue.oldValue, depth)}\n${currentIndent
+          .slice(0, -2)}+ ${currentValue.key}: ${handleCurrentValue(currentValue.newValue, depth)}`;
+      }
+      return `${currentIndent.slice(0, -2)}  ${currentValue.key}: ${iterateThroughValues(currentValue.children, depth + 1)}`;
+    });
+
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
+};
+
+const makeStylish = (arr) => iterateThroughValues(arr, 1);
 
 export default makeStylish;
